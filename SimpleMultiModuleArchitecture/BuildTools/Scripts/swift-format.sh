221a4b11e08244b123a8aa91f1b5a2ef3baaf2f2
Shinya Okazaki <negichou@gmail.com>
2024-05-12 04:38:08 +0900

    [fix] Xcode の BuildPhases で実行する swift-format.sh を更新
    元々、BuildPhases で swift-format のバイナリがなければビルドしようとしていたが、その際に以下のエラーがでることを確認。
    ・'buildtools': Invalid manifest エラー
    
    おそらく 「xcrun --sdk maxosx swift build ...」というコマンドを実行する際に何かがエラーになっていると思うが、原因不明。
    このため、cliツールのビルドはコマンドラインで各自が行うようにする方針

--- SimpleMultiModuleArchitecture/BuildTools/Scripts/swift-format.sh
+++ SimpleMultiModuleArchitecture/BuildTools/Scripts/swift-format.sh
@@ -5,31 +5,28 @@
 echo $PROJECT_DIR
 cd $PROJECT_DIR
 
-# コマンドが存在するかどうかを確認する
-if [ ! -e '../BuildTools/.build/release/swift-format' ]; then
-    echo 'swift-format not exist. -> build...'
-    xcrun --sdk macosx swift build -c release --package-path ../BuildTools
-    echo 'swift-format build success.'
+if [ -e '../BuildTools/.build/release/swift-format' ]; then
+
+    # .swift-format 設定ファイルをもとにフォーマッターをかける
+    ## Note: コマンドオプションについて
+    ##   -p: 並列に実行
+    ##   -i: フォーマット時に現在のファイルを上書きする
+    ##   -r: フォルダを再帰的に確認
+    
+    # 注意点: Package.swift に formatter をかけると、ビルド時に更新されてしまう
+    # -> `resolving package graph` で毎回待たされることになるため、避けること
+    # ../BuildTools/.build/release/swift-format format --configuration ../BuildTools/.swift-format -p -i -r $PROJECT_DIR/../Modules/Sources/ $PROJECT_DIR/../Modules/Package.swift
+    ../BuildTools/.build/release/swift-format format --configuration ../BuildTools/.swift-format -p -i -r $PROJECT_DIR/../Modules/Sources/
+    
+    # .swift-format 設定ファイルをもとに Lint をかける
+    ## Note: コマンドオプションについて
+    ##   -p: 並列に実行
+    ##   -r: フォルダを再帰的に確認
+    # ../BuildTools/.build/release/swift-format lint --configuration ../BuildTools/.swift-format -p -r $PROJECT_DIR/../Modules/Sources/ $PROJECT_DIR/../Modules/Package.swift
+    ../BuildTools/.build/release/swift-format lint --configuration ../BuildTools/.swift-format -p -r $PROJECT_DIR/../Modules/Sources/
+    
+    echo 'complete swift-format.'
+    
 else
-    echo 'swift-format exist.'
+    echo 'swift-format does not exist. Run the make command.'
 fi
-
-# .swift-format 設定ファイルをもとにフォーマッターをかける
-## Note: コマンドオプションについて
-##   -p: 並列に実行
-##   -i: フォーマット時に現在のファイルを上書きする
-##   -r: フォルダを再帰的に確認
-
-# 注意点: Package.swift に formatter をかけると、ビルド時に更新されてしまう
-# -> `resolving package graph` で毎回待たされることになるため、避けること
-# ../BuildTools/.build/release/swift-format format --configuration ../BuildTools/.swift-format -p -i -r $PROJECT_DIR/../Modules/Sources/ $PROJECT_DIR/../Modules/Package.swift
-../BuildTools/.build/release/swift-format format --configuration ../BuildTools/.swift-format -p -i -r $PROJECT_DIR/../Modules/Sources/
-
-# .swift-format 設定ファイルをもとに Lint をかける
-## Note: コマンドオプションについて
-##   -p: 並列に実行
-##   -r: フォルダを再帰的に確認
-# ../BuildTools/.build/release/swift-format lint --configuration ../BuildTools/.swift-format -p -r $PROJECT_DIR/../Modules/Sources/ $PROJECT_DIR/../Modules/Package.swift
-../BuildTools/.build/release/swift-format lint --configuration ../BuildTools/.swift-format -p -r $PROJECT_DIR/../Modules/Sources/
-
-echo 'complete swift-format.'
